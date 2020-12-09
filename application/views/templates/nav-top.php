011c30d63c13f436038bd1535e6b761a8f16b691
Giovane <giovaneaalmeida27@gmail.com>
2020-12-09 13:02:41 -0300

    Campo de busca removido

--- application/views/templates/nav-top.php
+++ application/views/templates/nav-top.php
@@ -1,10 +1,5 @@
 <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
   <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Cadastro de Alunos</a>
-	<div>
-		<form action="" method="post">
-			<input class="form-control form-control-dark" type="text" name="busca" id="busca" placeholder="Search" aria-label="Search" value="">
-		</form>
-	</div>
 </nav>
 
 <div class="container-fluid">
