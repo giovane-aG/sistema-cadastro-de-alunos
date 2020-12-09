b34d9521f749dfd7f2bda2c9bf5b7eae28cbfe0b
Giovane <giovaneaalmeida27@gmail.com>
2020-12-09 14:55:05 -0300

    Readme e arquivo de banco de dados adicionados

---
+++ README.md
@@ -0,0 +1,5 @@
+# Como utilizar o sistema
+
+1 - Para utilizar o sistema é necessário ter o xampp instalado e mover a pasta raiz do projeto para a pasta htdocs.
+2 - É necessário criar uma base de dados chamada alunos e nela colar as queries do arquivo banco-de-dados.sql,localizado na raiz do projeto.
+3 - Após ligar o apache e o mysql no xampp, acessar o endereço http://localhost/crud_alunos/.
---
+++ banco-de-dados.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 09-Dez-2020 às 18:32
+-- Versão do servidor: 10.4.17-MariaDB
+-- versão do PHP: 8.0.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `alunos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `alunos`
+--
+
+CREATE TABLE `alunos` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `endereco` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `foto` varchar(50) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Extraindo dados da tabela `alunos`
+--
+
+INSERT INTO `alunos` (`id`, `nome`, `endereco`, `foto`) VALUES
+(17, 'Grogu', 'Uma galáxia distante', 'uploads/grogu.png'),
+(18, 'Snake', 'Avenida Fox Hound', 'uploads/MGS4OldSnake.jpg'),
+(19, 'Giovane Aguiar', 'rua Oito', 'uploads/brad.png'),
+(20, 'Dash Berlin', 'Berlin', 'uploads/dashBerlin.jpg');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `alunos`
+--
+ALTER TABLE `alunos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `alunos`
+--
+ALTER TABLE `alunos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
