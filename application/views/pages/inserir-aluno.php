e508522dccee9d54764755f8cd9d4bc5bf3cf64a
Giovane <giovaneaalmeida27@gmail.com>
2020-12-09 09:29:45 -0300

    Lógica para editar ou cadastrar aluno

--- application/views/pages/inserir-aluno.php
+++ application/views/pages/inserir-aluno.php
@@ -1,43 +1,42 @@
     <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
       <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">Cadastrar Aluno</h1>
-        <div class="btn-toolbar mb-2 mb-md-0">
-          <div class="btn-group mr-2">
-            <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
-            <button type="button" class="btn btn-sm btn-outline-secondary">Export</button>
-          </div>
-          <button type="button" class="btn btn-sm btn-outline-secondary dropdown-toggle">
-            <span data-feather="calendar"></span>
-            This week
-          </button>
-        </div>
+				<?php if(isset($aluno)) : ?>
+					<h1 class="h2">Editar Aluno</h1>
+				<?php else : ?>
+					<h1 class="h2">Cadastrar Aluno</h1>
+				<?php endif; ?>
       </div>
 
 			<div class="col-md-12">
-				<form enctype="multipart/form-data" action="<?= base_url() ?>aluno/salvarAluno" method="post">
+				<?php if(isset($aluno)) : ?>
+					<form enctype="multipart/form-data" action="<?= base_url() ?>aluno/atualizarAluno/<?= $aluno['id'] ?>" method="post">
+				<?php else : ?>
+					<form enctype="multipart/form-data" action="<?= base_url() ?>aluno/salvarAluno" method="post">
+				<?php endif; ?>
+
 					<div class="col-md-6">
 						<div class="form-group">
 							<label for="name">Nome</label>
-							<input type="text" class="form-control" name="nome" id="name" placeholder="Name" value="">
+							<input type="text" required class="form-control" name="nome" id="name" placeholder="Name" value="<?= isset($aluno) ? $aluno["nome"] : "" ?>">
 						</div>
 					</div>
 
 					<div class="col-md-6">
 						<div class="form-group">
 							<label for="description">Endereço</label>
-							<input name="endereco" id="description" class="form-control"></input>
+							<input type="text" name="endereco" required  id="description" class="form-control" value="<?= isset($aluno) ? $aluno["endereco"] : "" ?>"></input>
 						</div>
 					</div>
 
 					<div class="col-md-6">
 						<div class="form-group">
 							<label for="category">Foto</label>
-							<input type="file" class="form-control" name="foto" id="category" placeholder="Category" value="">
+							<input type="file" class="form-control" name="foto" id="category" placeholder="Category" value="<?= isset($aluno) ? $aluno["foto"] : "" ?>">
 						</div>
 					</div>
 					<div class="col-md-6">
 							<button type="submit" class="btn btn-success btn-xs"><i class="fas fa-check"></i> Save</button>
-							<a href="" class="btn btn-danger btn-xs"><i class="fas fa-times"></i> Cancel</a>
+							<a href="<?= base_url() ?>aluno" class="btn btn-danger btn-xs"><i class="fas fa-times"></i> Cancel</a>
 						</div>
 					</div>
 				</form>
