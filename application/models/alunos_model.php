e1368b2607eb787549221c43712563b58106720d
Giovane <giovaneaalmeida27@gmail.com>
2020-12-09 08:13:15 -0300

    Funções inserir e mostrar criadas

--- application/models/alunos_model.php
+++ application/models/alunos_model.php
@@ -5,4 +5,18 @@ class Alunos_model extends CI_MODEL {
   public function index() {
     return $this->db->get("alunos")->result_array();
   }
+
+  public function inserir($data) {
+    return $this->db->insert('alunos', $data);
+  }
+
+  	public function mostrar($id) {
+		return $this->db->get_where('alunos', array('id' => $id))->row_array();
+	}
 }
+
+
+// standart class object
+// $nome = new stdClass();
+// $nome->nome = 'josvane';
+// $nome->endereco = 'rua oito';
